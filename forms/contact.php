c03f7ef464acca55669411a465c6895c92733a28
LeonardoLombana <leonardo.lc02@hotmail.com>
2025-05-06 15:30:33 -0500

    Prueba configuración servicio de envio de correos smtp

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.gmail.com',
+    'username' => 'leonardo.lc02014@gmail.com',
+    'password' => 'L30n4rd01985',
+    'port' => '465'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
